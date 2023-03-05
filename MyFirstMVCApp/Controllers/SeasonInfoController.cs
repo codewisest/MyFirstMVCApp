d3b0187c8b8de51cd56337d278382e54cde47717
Chijioke <wisestcodes@gmail.com>
2023-03-04 23:24:53 -0500

    Add project files.

---
+++ MyFirstMVCApp/Controllers/SeasonInfoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyFirstMVCApp.Models;
+
+namespace MyFirstMVCApp.Controllers
+{
+    public class SeasonInfoController : Controller
+    {
+        // GET: SeasonInfo
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Wisest Home";
+            return View();
+        }
+
+        public ActionResult Season(int? temperature)
+        {
+            ViewBag.Temp = temperature;
+            if (temperature == null)
+            {
+                ViewBag.Temp = "unknown";
+            }
+
+            if (temperature == null) ViewBag.Season = "unkbown";
+            else
+            {
+                TemperatureIfController temperatureIfController = new TemperatureIfController();
+                ViewBag.Season = temperatureIfController.WhatSeason((int)temperature);
+                //TemperatureIfController temperatureIfController = new TemperatureIfController();
+                //ViewBag.Season = temperatureIfController.WhatSeason((int)temperature);
+            }
+            return View();
+        }
+    }
+}
---
+++ MyFirstMVCApp/Models/TemperatureIfController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVCApp.Models
+{
+    public class TemperatureIfController : Controller
+    {
+        /// <summary>
+        /// Returns the season based on the input temperature
+        /// </summary>
+        /// <param name="temp">in put temperature</param>
+        /// <returns>string value of the season</returns>
+        // [HttpGet]
+        // [Route("api/temperatureif/whattemperature/{temp}")]
+        public string WhatSeason(int temp)
+        {
+            if (temp > 20) return "Summer!";
+            else if (temp > 15 && temp <= 20) return "Fall!";
+            else if (temp > 10 && temp <= 15) return "Spring";
+            else return "Winter";
+        }
+
+    }
+}
